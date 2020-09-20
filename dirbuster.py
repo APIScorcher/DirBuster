29848603be885c6850275e5b8baecd72a9dc8744
APIScorcher <63946837+APIScorcher@users.noreply.github.com>
2020-09-20 15:07:24 +0500

    Create dirbuster.py

---
+++ dirbuster.py
@@ -0,0 +1,29 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u", type=str, dest="url", help="--url (https://url.com)", default=None)
+parser.add_argument("-w", type=str, dest="wordlist", help="--wordlist", default=None)
+
+args = parser.parse_args()
+url = ""
+wordlist = ""
+
+url = args.url
+wordlist = args.wordlist
+
+def dirb(url, dict):
+    try:
+        wordlist = open(dict,"rb")
+        for path in wordlist.readlines():
+            path = path.strip().decode("utf-8")
+            urlpath = url+"/"+path
+            r = requests.get(urlpath)
+            if r.status_code != 404:
+                print("{} -> {}".format(r.status_code, urlpath))
+    except: # Catching exceptions
+        print("Error Occured!")
+
+if __name__ == "__main__":
+    dirb(url, wordlist)
